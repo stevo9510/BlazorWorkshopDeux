7b37ef633e98e627099788073c9a87c848f3cd39
stevo9510 <stevo9510@gmail.com>
2019-12-21 01:39:08 -0500

    Add project files.

---
+++ BandBookerDataTest/Program.cs
@@ -0,0 +1,36 @@
+﻿using BandBookerData;
+using BandBookerData.Models;
+using System;
+using System.Linq;
+
+namespace BandBookerDataTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Un-comment to start over
+            //DataManager.DeleteAll();
+
+            AddNewInstrument("Guitar");
+            AddNewInstrument("Keyboards");
+        }
+
+        private static void AddNewInstrument(string instrumentName)
+        {
+            var instrument = (from i in DataManager.Instruments
+                          where i.Name == instrumentName
+                          select i).FirstOrDefault();
+
+            if (instrument == null)
+            {
+                instrument = new Instrument()
+                {
+                    Name = instrumentName
+                };
+                DataManager.AddInstrument(instrument);
+            }
+            Console.WriteLine("Instrument: {0} with InstrumentId {1}", instrument.Name, instrument.InstrumentId);
+        }
+    }
+}
