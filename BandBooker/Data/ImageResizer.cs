30d5fd6b678dde13e02efc93a971c1ed140e019b
stevo9510 <stevo9510@gmail.com>
2019-12-27 23:27:23 -0500

    Added Musician code from demo

---
+++ BandBooker/Data/ImageResizer.cs
@@ -0,0 +1,47 @@
+﻿using LazZiya.ImageResize;
+using System;
+using System.Drawing;
+
+namespace BandBooker.Data
+{
+    public class ImageResizer
+    {
+        public static void ResizeAndSaveImage(string inputFile, int maxWidth)
+        {
+            var img = Image.FromFile(inputFile);
+            var extension = System.IO.Path.GetExtension(inputFile);
+
+            string tempfile = Environment.CurrentDirectory
+                + "\\" + DateTime.Now.Ticks.ToString()
+                + extension;
+
+            int maxHeight = img.Height;
+
+            if (maxWidth == 0)
+                maxWidth = img.Width;
+
+            var ratioX = (double)maxWidth / img.Width;
+            var ratioY = (double)maxHeight / img.Height;
+            var ratio = Math.Min(ratioX, ratioY);
+
+            var newWidth = (int)(img.Width * ratio);
+            var newHeight = (int)(img.Height * ratio);
+
+            //resize the image to 600x400 
+            var newImg = ImageResize.Scale(img, newWidth, newHeight);
+
+            //save new image
+            newImg.SaveAs(tempfile);
+
+            //dispose to free up memory
+            img.Dispose();
+            newImg.Dispose();
+
+            // delete original file
+            System.IO.File.Delete(inputFile);
+
+            // rename tempfile
+            System.IO.File.Move(tempfile, inputFile);
+        }
+    }
+}
--- BandBooker/Startup.cs
+++ BandBooker/Startup.cs
@@ -1,20 +1,14 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using BandBooker.Areas.Identity;
+using BandBooker.Data;
+using Blazor.FileReader;
 using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Authorization;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Identity.UI;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using BandBooker.Areas.Identity;
-using BandBooker.Data;
 
 namespace BandBooker
 {
@@ -40,6 +34,7 @@ namespace BandBooker
             services.AddServerSideBlazor();
             services.AddScoped<AuthenticationStateProvider, RevalidatingIdentityAuthenticationStateProvider<IdentityUser>>();
             services.AddSingleton<WeatherForecastService>();
+            services.AddFileReaderService(options => options.InitializeOnFirstCall = true);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
